673c0425992dc0677eb30bc7a6e8b336efac0853
bugszhou <bugszhou@outlook.com>
2019-03-13 14:42:50 +0800

    feat(all): add commitlint

--- meta.js
+++ meta.js
@@ -3,60 +3,61 @@
  */
 
 module.exports = {
-    
-    prompts: {
-        name: {
-            type: 'string',
-            required: true,
-            message: 'Project name',
-        },
-        description: {
-            type: 'string',
-            required: false,
-            message: 'Project description',
-            default: 'A aliapp(支付宝小程序) project with ziu',
-        },
-        author: {
-            type: 'string',
-            message: 'Author',
-        },
-        tab: {
-            type: 'confirm',
-            message: 'Use Tab?',
-        },
-        lint: {
-            type: 'confirm',
-            message: 'Use ESLint to lint your code?',
-        },
-        lintConfig: {
-            when: 'lint',
-            type: 'list',
-            message: 'Pick an ESLint preset',
-            choices: [
-                // {
-                //     name: 'Standard (https://github.com/standard/standard)',
-                //     value: 'standard',
-                //     short: 'Standard',
-                // },
-                {
-                    name: 'Airbnb (https://github.com/airbnb/javascript)',
-                    value: 'airbnb',
-                    short: 'Airbnb',
-                },
-                // {
-                //     name: 'none (configure it yourself)',
-                //     value: 'none',
-                //     short: 'none',
-                // },
-            ],
-        },
-        // sass: {
-        //     type: 'confirm',
-        //     message: 'Use sass?'
-        // }
+
+  prompts: {
+    name: {
+      type: 'string',
+      required: true,
+      message: 'Project name',
     },
-    filters: {
-        'scripts/.eslintrc.js': 'lint',
-        'scripts/.eslintignore': 'lint'
-    }
+    description: {
+      type: 'string',
+      required: false,
+      message: 'Project description',
+      default: 'A aliapp(支付宝小程序) project with ziu',
+    },
+    author: {
+      type: 'string',
+      message: 'Author',
+    },
+    tab: {
+      type: 'confirm',
+      message: 'Use Tab?',
+    },
+    lint: {
+      type: 'confirm',
+      message: 'Use ESLint to lint your code?',
+    },
+    lintConfig: {
+      when: 'lint',
+      type: 'list',
+      message: 'Pick an ESLint preset',
+      choices: [
+        // {
+        //     name: 'Standard (https://github.com/standard/standard)',
+        //     value: 'standard',
+        //     short: 'Standard',
+        // },
+        {
+          name: 'Airbnb (https://github.com/airbnb/javascript)',
+          value: 'airbnb',
+          short: 'Airbnb',
+        },
+        // {
+        //     name: 'none (configure it yourself)',
+        //     value: 'none',
+        //     short: 'none',
+        // },
+      ],
+    },
+    // sass: {
+    //     type: 'confirm',
+    //     message: 'Use sass?'
+    // }
+  },
+  filters: {
+    'scripts/.eslintrc.js': 'lint',
+    'scripts/.eslintignore': 'lint',
+    'commitlint.config.js': 'gitCommitMsg'
+  }
 };
